a28399829a095f424da86d445888319e4a8c126c
sutulo.vladlen <sutulo.vlad@gmail.com>
2023-02-07 00:15:39 +0300

    hw3 task1 method count

--- homework3/task_1.py
+++ homework3/task_1.py
@@ -10,7 +10,7 @@ A[1..N]. Пользователь в первой строке вводит на
 from random import randint
 leng = int (input("Введите длинну списка"))
 desird_number =  int (input("Введите число количество которого мы ищем"))
-listt = []
+listt = [] #можно создать список заданного размера
 caunt = 0
 for i in range(leng):
     listt.append(randint(0,10))
@@ -19,4 +19,9 @@ for i in listt:
     if i == desird_number:
         caunt += 1
 print(f"число {desird_number} всречается {caunt} раз")
+
+count = listt.count(desird_number)
+print(f"число {desird_number} всречается {caunt} раз")
+
+
 
